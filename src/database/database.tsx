187e16c7c3404e5776749f98d949e753f52a9e49
megicula27 <aditya1272001@gmail.com>
2024-05-16 22:36:34 +0530

    database added

---
+++ src/database/database.tsx
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+const dbconnection = async (): Promise<void> => {
+  if (connection.isConnected) {
+    console.log("Database already connected");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {
+      dbName: "pegasus",
+    });
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log(db);
+
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database connection failed", error);
+    process.exit(1);
+  }
+};
+
+export default dbconnection;
